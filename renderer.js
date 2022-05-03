9487880c2474ad7674481d51ac6b273dc841d150
luis.iglesias <luisjavi27@gmail.com>
2022-05-02 23:38:06 -0500

    funcionalidad ok, faltan detalles menores de estilo y UX

--- renderer.js
+++ renderer.js
@@ -1,35 +1,31 @@
 let items = [1];
 
-let producto = {
-  titulo: "",
-  subtitulo: "",
-  descripcion: "",
-  urlImagen: "",
-};
-
-producto.titulo = document.querySelector("#inputTitulo");
-producto.subtitulo = document.querySelector("#subtitulo1");
-producto.descripcion = document.querySelector("#inputDescripcion1");
-producto.urlImagen = document.querySelector("#urlImageInput");
+titulo = document.querySelector("#inputTitulo");
+urlImagen = document.querySelector("#urlImageInput");
 containerItems = document.querySelector("#itemsArea");
-btnPlus = document.querySelector("#addButton");
+addButton = document.querySelector("#addButton");
 btnCargarUrlImagen = document.querySelector("#btnCargarUrlImagen");
-btnCargarUrlImagen = document.querySelector("#containerCopyIcon");
+btnCopy = document.querySelector("#containerCopyIcon");
+btnGenerateHtml = document.querySelector("#containerGenerateIcon");
+inputUrlImagen = document.querySelector("#urlImageInput");
+outputHtml = document.querySelector("#outputHtml");
 
 agregarEventListeners();
 
 function agregarEventListeners() {
-  btnPlus.addEventListener("mousedown", () => {
+  addButton.addEventListener("mousedown", () => {
     addItem(items);
   });
+
+  btnCargarUrlImagen.addEventListener("click", cargarImagen);
+  btnCopy.addEventListener("click", copiarHtml);
+  btnGenerateHtml.addEventListener("click", generarHtml);
 }
 
 function addItem(index) {
-
   let lastItem = index[index.length - 1];
   let newitem = lastItem + 1;
   items.push(newitem);
-
 
   containerItems.insertAdjacentHTML(
     "beforeend",
@@ -47,14 +43,59 @@ function addItem(index) {
   addDeleteListener(newitem);
 }
 
-function addDeleteListener(item){
-
-    document.getElementById(`deleteItem${item}`).addEventListener('mousedown',()=>{
-        document.getElementById(`containerDeleteItem${item}`).remove();
+function addDeleteListener(item) {
+  document
+    .getElementById(`deleteItem${item}`)
+    .addEventListener("mousedown", () => {
+      document.getElementById(`containerDeleteItem${item}`).remove();
     });
+}
 
-    
+function cargarImagen() {
+  let image = document.querySelector("#imageArea img");
+  image.setAttribute("src", inputUrlImagen.value);
+}
+
+function copiarHtml() {
+ 
+    outputHtml.select();
+    document.execCommand('copy');
 
 }
 
-
+function generarHtml() {
+    let stringTitulo = titulo.value;
+    let subtitulos = [];
+    let descripciones =[];
+    let stringHtmlImage = "";
+  
+    items.forEach(function (item) {
+      subtitulos.push(document.getElementById(`subtitulo${item}`).value);
+      descripciones.push(document.getElementById(`inputDescripcion${item}`).value);
+    });
+  
+    image = document.querySelector("#imageArea img");
+    stringHtmlImage = image.getAttribute("src");
+  
+    let resultString = `
+      <p>
+      <br><b>${stringTitulo}</b><br>`;
+  
+      for(let i = 0; i <=  (items.length - 1 ); i++){
+  
+          resultString += `
+          <br><b>• ${subtitulos[i]}: </b>${descripciones[i]}`;
+      }
+  
+      resultString += 
+      `</p>
+      <br>
+      <div  style="text-align: center;">
+          <img style=" height:100px;" src="${stringHtmlImage}">
+      </div>
+      `
+  
+      outputHtml.value= resultString;
+  
+      
+}
